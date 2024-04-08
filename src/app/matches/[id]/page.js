8291afa7a690f695151ff70a206128b7e621ff98
Vansh Bordia <vanshbordia@gmail.com>
2024-04-08 17:49:39 +0530

    v0.1.5

--- src/app/matches/[id]/page.js
+++ src/app/matches/[id]/page.js
@@ -6,7 +6,7 @@ import StatsPlayerRow from '@/components/StatsPlayerRow';
 
 async function ParentComponent({ params }) {
 
-    const matchResponse = await fetch(`https://ap.api.riotgames.com/val/match/v1/matches/${params.id}?api_key=${riotAPI}`);
+    const matchResponse = await fetch(`https://ap.api.riotgames.com/val/match/v1/matches/${params.id}?api_key=${process.env.riotAPI}`);
     const matchData = await matchResponse.json();
 
     return (
