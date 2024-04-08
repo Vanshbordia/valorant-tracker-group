a7210107bf7d3dec9787a1adbae474cf57b35e8d
Vansh Bordia <vanshbordia@gmail.com>
2024-04-08 17:22:30 +0530

    v0.1.1

--- src/components/StatsPlayerRow.js
+++ src/components/StatsPlayerRow.js
@@ -17,13 +17,12 @@ export default function StatsPlayerRow({ matchData }) {
     console.log(didWin)
     const mapUrlParts = matchData.matchInfo.mapId.split('/');
     const lastName = mapUrlParts[mapUrlParts.length - 1];
-    console.log(getMapsImage(lastName))
 
     const titles = [' ', 'Game Name', 'Score', 'KDA'];
     return (
         <>
             <div className="w-full h-56 text-5xl font-bold flex flex-row align-middle justify-center relative bg-black">
-                <img src={getMapsImage(lastName)} className="absolute inset-0 w-full h-full object-cover opacity-50 bg-black" />
+                <img src={GetMap(lastName)} className="absolute inset-0 w-full h-full object-cover opacity-50 bg-black" />
                 <div className="absolute inset-0 flex items-center justify-center text-white  flex-col gap-2">
                     {ourteam === "Blue" ? (
                         <p>{matchData.teams[1].roundsWon}:{matchData.teams[0].roundsWon}</p>
@@ -54,7 +53,7 @@ export default function StatsPlayerRow({ matchData }) {
                     {/* Render player stats */}
                     {sortedPlayers.map((player, index) => (
                         <tr key={index} className={player.teamId === 'Blue' ? 'bg-green-100' : 'bg-red-100'}>
-                            <td className="px-1 py-2 border-t "><img src={getImage(player.characterId)} className="w-10 h-10 aspect-square min-w-fit" alt="" /></td>
+                            <td className="px-1 py-2 border-t "><img src={GetImage(player.characterId)} className="w-10 h-10 aspect-square min-w-fit" alt="" /></td>
                             <td className="px-2 py-2 border-t  ">{player.gameName}</td>
                             <td className="px-1 py-2 border-t text-center">{player.stats.score}</td>
                             <td className="px-1 py-2 border-t  text-center">{player.stats.kills} / {player.stats.deaths} / {player.stats.assists} </td>
@@ -79,7 +78,7 @@ export default function StatsPlayerRow({ matchData }) {
                     {/* Render player stats */}
                     {ourplayer.map((player, index) => (
                         <tr key={index} className={player.teamId === 'Blue' ? 'bg-green-100' : 'bg-red-100'}>
-                            <td className="px-1 py-2 border-t border-l border-gray-200"><img src={getImage(player.characterId)} className="w-10 h-10 aspect-square min-w-fit" alt="" /></td>
+                            <td className="px-1 py-2 border-t border-l border-gray-200"><img src={GetImage(player.characterId)} className="w-10 h-10 aspect-square min-w-fit" alt="" /></td>
                             <td className="px-2 py-2 border-t border-l border-gray-200">{player.gameName}</td>
                             <td className="px-1 py-2 border-t border-l border-gray-200">{player.stats.score}</td>
                             <td className="px-1 py-2 border-t border-l border-gray-200">{player.stats.kills} / {player.stats.deaths} / {player.stats.assists}</td>
@@ -98,7 +97,7 @@ export default function StatsPlayerRow({ matchData }) {
 }
 
 
-function getImage(characterId) {
+export function GetImage(characterId) {
     const [agentData, setAgentData] = useState(null);
     useEffect(() => {
         fetch(`https://valorant-api.com/v1/agents/${characterId}`)
@@ -115,7 +114,7 @@ function getImage(characterId) {
 }
 
 
-function getMapsImage(mapid) {
+export function GetMap(mapid) {
     const [mapData, setMapData] = useState(null);
 
     useEffect(() => {
